38993ac8426d71edabb09f46e791d638e9864b6b
Rodrigo Caetano Reynaldo <pp5ere@gmail.com>
2020-12-24 12:50:30 -0300

    inserted a new varible into struct

--- repository/txtfile.go
+++ repository/txtfile.go
@@ -1,9 +1,10 @@
 package repository
 
 import (
+	"bufio"
+	"errors"
 	"filechallenge/entity"
 	"filechallenge/util"
-	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -16,6 +17,8 @@ import (
 //TxtFile store the file path
 type TxtFile struct{
 	FilePath string
+	Lines [] string
+	Command string
 }
 
 //ReadFile return a file
@@ -31,6 +34,49 @@ func (txt *TxtFile) RemoveFile() error {
 	if err := os.Remove(txt.FilePath); err != nil{
 		return err
 	}
+	return nil
+}
+
+//ProcessFile scan whole file transform and insert into database
+func (txt *TxtFile) ProcessFile(file *os.File) {		
+	scanner := bufio.NewScanner(file)
+	defer file.Close()
+	scanner.Scan()
+    for scanner.Scan() {
+		line := RemoveSpaces(scanner.Text())
+		txt.Lines = append(txt.Lines, line)
+    }	
+}
+
+//ParseToStruct parse the string line to struct
+func (txt *TxtFile) ParseToStruct(line string, sales *entity.SalesData) error {
+	arr := strings.Split(line, ";")
+	if len(arr) == 8 {
+		cpf 		:= util.GetOnlyNumbers(arr[0])
+		isValidCpf 	:= util.CpfIsValid(cpf)		
+		prv, err := util.GetBoolean(arr[1]); if err != nil{
+			return err
+		}
+		inc, err := util.GetBoolean(arr[2]); if err != nil{
+			return err
+		}
+		date := util.GetDate(arr[3])
+		tktMedio := util.GetFloat(arr[4])
+		tktUlt := util.GetFloat(arr[5])
+		sales.Cpf 							= cpf
+		sales.CpfValido						= isValidCpf
+		sales.Private 						= prv
+		sales.Incompleto 					= inc
+		sales.DataDaUltimaCompra 			= date
+		sales.TicketMedio 					= tktMedio
+		sales.TicketDaUltimaCompra			= tktUlt
+		sales.LojaMaisFrequente 			= util.GetOnlyNumbers(arr[6])
+		sales.CnpjLojaMaisFrequenteValido 	= util.CnpjIsValid(sales.LojaMaisFrequente)
+		sales.LojaDaUltimaCompra 			= util.GetOnlyNumbers(arr[7])
+		sales.CnpjLojaDaUltimaCompraValido	= util.CnpjIsValid(sales.LojaDaUltimaCompra)
+	}else{
+		return errors.New(fmt.Sprint("Cannot parse to struct because columns number must be ", 8, arr))
+	}	
 	return nil
 }
 
@@ -61,35 +107,4 @@ func SaveFile(w http.ResponseWriter, r *http.Request)(*TxtFile ,error) {
 func RemoveSpaces(line string) string{
 	space := regexp.MustCompile(`\s+`)
 	return space.ReplaceAllString(line, ";")
-}
-//ParseToStruct parse the string line to struct
-func ParseToStruct(line string, txt *entity.SalesData) error {
-	arr := strings.Split(line, ";")
-	if len(arr) == 8 {
-		cpf 		:= util.GetOnlyNumbers(arr[0])
-		isValidCpf 	:= util.CpfIsValid(cpf)		
-		prv, err := util.GetBoolean(arr[1]); if err != nil{
-			return err
-		}
-		inc, err := util.GetBoolean(arr[2]); if err != nil{
-			return err
-		}
-		date := util.GetDate(arr[3])
-		tktMedio := util.GetFloat(arr[4])
-		tktUlt := util.GetFloat(arr[5])
-		txt.Cpf 						= cpf
-		txt.CpfValido					= isValidCpf
-		txt.Private 					= prv
-		txt.Incompleto 					= inc
-		txt.DataDaUltimaCompra 			= date
-		txt.TicketMedio 				= tktMedio
-		txt.TicketDaUltimaCompra		= tktUlt
-		txt.LojaMaisFrequente 			= util.GetOnlyNumbers(arr[6])
-		txt.CnpjLojaMaisFrequenteValido = util.CnpjIsValid(txt.LojaMaisFrequente)
-		txt.LojaDaUltimaCompra 			= util.GetOnlyNumbers(arr[7])
-		txt.CnpjLojaDaUltimaCompraValido= util.CnpjIsValid(txt.LojaDaUltimaCompra)
-	}else{
-		return errors.New(fmt.Sprint("Cannot parse to struct because columns number must be ", 8, arr))
-	}	
-	return nil
 }
